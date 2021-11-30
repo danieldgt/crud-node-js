980ee521447baf2ebf17f1290b9760256631df1b
Daniel Alencar Barros Tavares <danielalencarbarrostavares@MacBook-Pro-de-Daniel.local>
2021-11-30 15:19:06 -0300

    /Serve para fazer um hotdeploy da aplicação ao salvar algum arquivo. executar como: yarn dev

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express'); // importa o express
 
 function hello(argument) {
-	// body...
 	console.log(`Olá NodeJS!! Você está utilizando ${process.version} do NodeJS.`);
 }
 
